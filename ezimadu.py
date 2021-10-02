cd0f9f179c94d5c9229945d3e6aa33910f9460b5
lacey-e <buchiezim@gmail.com>
2021-10-02 16:56:53 +0100

    new

---
+++ ezimadu.py
@@ -0,0 +1,11 @@
+def main():
+    greet = intro()
+    print(greet)
+
+
+
+def intro():
+    print("hello from Esther") 
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bontia import greetings
 from jularbal import greeting
-
+from ezimadu import intro
 print("Welcome to our collaborative progam!")
 greetings()
 greeting()
+intro ()
